f8e84691fee4c7b5dbc62d46c3cd43bd8c4f7237
raviteja-athmakuri <raviteja.athmakuri@gmail.com>
2023-02-21 17:05:11 +0530

    Overriding

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,13 +2,13 @@ package com.driver;
 
 public class Main {
     public static class A{
-        String meth(){
+        public String meth(){
             return "Invoking method from class A";
         }
     }
-    static class B extends A{
+    public static class B extends A{
 
-        String meth(){
+       public  String meth(){
             return "Method is overridden in Extendend class B";
         }
 
